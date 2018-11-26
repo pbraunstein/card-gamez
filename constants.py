0fe9cc9f7d91c0ebd2a1490a7f447f6853665fe5
pbraunstein <pbraunstein12@gmail.com>
2018-11-26 08:10:11 -0500

    Added Rank enum

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-from enum import Enum, auto,  unique
+from enum import Enum, IntEnum, auto,  unique
 
 @unique
 class Suit(Enum):
@@ -6,4 +6,20 @@ class Suit(Enum):
     CLUB = auto()
     HEART = auto()
     DIAMOND = auto()
+
+@unique
+class Rank(IntEnum):
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
 
