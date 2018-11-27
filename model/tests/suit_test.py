932bdeae99347e2b688b5f6fd9b563abb827abac
pbraunstein <pbraunstein12@gmail.com>
2018-11-27 08:14:02 -0500

    suit unit tests

--- model/tests/suit_test.py
+++ model/tests/suit_test.py
@@ -1,8 +1,23 @@
 import unittest
 
+from model.suit import Suit
+
 class TestSuit(unittest.TestCase):
-    def test_one(self):
-        self.assertTrue(True)
+    def test_eq_operator(self):
+        self.assertTrue(Suit.DIAMOND == Suit.DIAMOND)
+        self.assertFalse(Suit.SPADE == Suit.HEART)
+
+    def test_ne_operator(self):
+        self.assertTrue(Suit.CLUB != Suit.SPADE)
+        self.assertFalse(Suit.HEART != Suit.HEART)
+
+    def test_eq_with_int_raises_error(self):
+        with self.assertRaises(TypeError):
+            Suit.SPADE == 1
+
+    def test_ne_with_int_raises_error(self):
+        with self.assertRaises(TypeError):
+            Suit.DIAMOND != 1
 
 if __name__ == '__main__':
     unittest.main()
