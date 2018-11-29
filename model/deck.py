fb4d3476d69e6bc707588e59c849f9d0c3c333a4
pbraunstein <pbraunstein12@gmail.com>
2018-11-29 07:56:03 -0500

    init all cards in deck

--- model/deck.py
+++ model/deck.py
@@ -1,10 +1,24 @@
+from random import shuffle
+
 from model.card import Card
+from model.rank import Rank
+from model.suit import Suit
 
 class Deck(object):
     def __init__(self):
+        self.cards = []
+        for rank in Rank:
+            for suit in Suit:
+                self.cards.append(Card(rank, suit))
         self._dealt = False
-
 
     @property
     def dealt(self):
         return self._dealt
+
+    @dealt.setter
+    def dealt(self, value):
+        self._dealt = value
+
+    def deal(self):
+        pass
