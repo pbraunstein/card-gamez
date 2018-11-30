bdb6f8a44f702fb2b22cc510e4004e0290febf62
pbraunstein <pbraunstein12@gmail.com>
2018-11-30 08:28:41 -0500

    leave todo describing next step

--- games/war/tests/war_test.py
+++ games/war/tests/war_test.py
@@ -2,6 +2,8 @@ import unittest
 
 from games.war.war import War
 
+# TODO: Make mock deck and have it return certain hands to test overall game
+
 class TestWar(unittest.TestCase):
     def test_wrong_deck_class_raises_error(self):
         with self.assertRaises(TypeError):
